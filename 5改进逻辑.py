f8c70c29392f2d65d1c6620477b692a2d42bd37f
Liu Yu <smaugfire@163.com>
2023-06-06 14:27:03 +0800

    new

--- 5改进逻辑.py
+++ 5改进逻辑.py
@@ -1,4 +1,5 @@
 from ctypes import windll
+from sympy.physics.units.definitions.unit_definitions import ms
 windll.shcore.SetProcessDpiAwareness(1)
 
 from tkinter import *
@@ -9,49 +10,33 @@ import pickle
 from tkinter import messagebox
 import tkinter.ttk as ttk
 
-def save_track(mouse_check_path,mouse_move_path,track_path):
-    '''
-    模拟轨迹
-    mouse_check_path:鼠标点击的位置和时间的二进制文件
-    mouse_move_path:鼠标移动的位置和时间的二进制文件
-    track_path:鼠标轨迹的二进制文件
-    鼠标点击和移动是列表，(t,(x,y))
-    
-    鼠标轨迹，是一个列表，包含每一段轨迹的列表，每一段列表由一个元组组成，
-    元组的第一个元素是记录时间，第二个元素是鼠标的x坐标，第三个元素是鼠标的y坐标(t,(x,y))
-    '''
-    divide_index=[]#记录鼠标移动和鼠标点击的分界点
-    with open(mouse_check_path,'rb') as file:
-        check_datas=pickle.load(file)
-    with open(mouse_move_path,'rb') as file:
-        move_datas=pickle.load(file)
-    print('点击点的个数为：',len(check_datas))
-    for i in range(len(check_datas)):
-        for j in range(len(move_datas)):
-            if check_datas[i][0]<move_datas[j][0]:
-                divide_index.append(j)
-                break
-    #得到切片的索引
-    track=[]
-    for i in range(0,len(divide_index)-1,2):
-        track.append(move_datas[divide_index[i]:divide_index[i+1]])
-    
-    print('总的轨迹长度为：',len(move_datas))
-    
-    for i in range(len(track)):
-        print(len(track[i]))
-    with open(track_path, "wb") as file:
-        pickle.dump(track, file)
-    #print(track)
-    print('轨迹保存成功')
 
 class App:
+    def read_data(self,D,W):
+        '''
+        通过D,和W的大小来区分和读取数据，，并返回四个数据结果。
+        '''
+        name=sys.path[0]+'\data\\'+'D='+str(D)+'_W='+str(W)
+        
+        with open(name+'_mouse_check.bin','rb') as file:
+            check_datas=pickle.load(file)
+        with open(name+'_mouse_move.bin','rb') as file:
+            move_datas=pickle.load(file)
+        with open(name+'_track.bin','rb') as file:
+            track_datas=pickle.load(file)
+        with open(name+'_information.bin','rb') as file:
+            inf=pickle.load(file)
+        
+        return move_datas,check_datas,track_datas,inf
+    
+    
+    
     def __init__(self, root):
         #setting title
         self.root=root
         self.root.title("undefined")
         #setting window size
-        self.width=1800
+        self.width=1600
         self.height=1200
         screenwidth = self.root.winfo_screenwidth()
         screenheight = self.root.winfo_screenheight()
@@ -89,7 +74,7 @@ class App:
         
         self.ID=StringVar()#难度系数，设置全局的变量
         ID_cb=ttk.Combobox(toolbar,textvariable=self.ID,font=ft,width=2)
-        ID_cb['value']=(2,3,4,5,6)
+        ID_cb['value']=(2,3,4,5,6,7,8)
         ID_cb.current(2)#设置默认值
         ID_cb.bind("<<ComboboxSelected>>",lambda event:self.W.set(int(int(self.D.get())*2/(2**int(self.ID.get())))))
         ID_cb.pack(pady=5,side=LEFT)
@@ -124,16 +109,30 @@ class App:
         spare_label=Label(toolbar,bg="#f0f0f0",font=ft,fg="#000000",justify="center",text="    ")
         spare_label.pack(pady=5,side=LEFT)
         
+        #根据设计的初始值加载数据
+        #将要保存的四个数据结构初始化为0，以便后续进行读取和处理
+        self.mouse_move,self.mouse_check,self.track,self.inf=self.read_data(self.D.get(),self.W.get())
+        
+        
         begin_game_btn=Button(toolbar,bg="#f0f0f0",font=ft,fg="#000000",justify="center",text="开始游戏",command=self.begin_game)    
         begin_game_btn.pack(pady=5,side=LEFT)
         show_trace_btn=Button(toolbar,bg="#f0f0f0",font=ft,fg="#000000",justify="center",text="显示动画",command=self.show_trace)
         show_trace_btn["bg"] = "#f0f0f0"
         show_trace_btn.pack(pady=5,side=LEFT)
         
+        #创建底部栏,用于显示一些标签信息
+        button_frame=Frame(self.root,relief=RAISED,borderwidth=1)
+        button_frame.pack(side=BOTTOM,fill=BOTH,padx=5,pady=5)
+        
+        ft = tkFont.Font(family='Times',size=10)
+        
+        
+        id_label=Label(button_frame,bg="#f0f0f0",font=ft,fg="#000000",justify="center",text="在这里可以显示状态信息")
+        id_label.pack(pady=5,side=LEFT)
         
         
         notebook=ttk.Notebook(self.root)
-        notebook.pack(fill=BOTH,expand=YES,padx=10,pady=10)
+        notebook.pack(fill=BOTH,expand=YES,padx=5,pady=0)
         # 创建画布
         canvas_frame1=Frame()
         self.canvas = Canvas(canvas_frame1,width=self.width, height=self.height,bg='white')
@@ -143,6 +142,14 @@ class App:
         notebook.add(canvas_frame1,text='轨迹动画')
         
         notebook.add(Frame(),text='轨迹分析')
+        
+        
+        
+        
+        
+        
+        
+        
         
     def newfile(self):
         messagebox.showinfo("New File", "New File")
@@ -156,9 +163,57 @@ class App:
     def about(self):
         messagebox.showinfo("About", "构建菲兹定律的GUI界面")
 
+    
+        
     def begin_game(self):
-        
-        
+        """
+        开始游戏,初始化游戏界面，并进行保存数据。
+        """
+        # 保存数据并退出
+        def save_esc():
+            #保存实验的关键信息
+            information_name = 'D='+str(self.inf['D'])+'_W='+str(self.inf['W'])+"_information.bin"  # 文件路径和名称
+            information_path=sys.path[0]+'\data\\'+information_name
+            # 打开文件，以写入模式
+            with open(information_path, "wb") as file:
+                pickle.dump(self.inf, file)
+            
+            mouse_move_name = 'D='+str(self.inf['D'])+'_W='+str(self.inf['W'])+"_mouse_move.bin"  # 文件路径和名称
+            mouse_move_path=sys.path[0]+'\data\\'+mouse_move_name
+            # 打开文件，以写入模式
+            with open(mouse_move_path, "wb") as file:
+                pickle.dump(self.mouse_move, file)
+            
+            mouse_check_name='D='+str(self.inf['D'])+'_W='+str(self.inf['W'])+"_mouse_check.bin"
+            mouse_check_path=sys.path[0]+'\data\\'+mouse_check_name
+            with open(mouse_check_path, "wb") as file:
+                pickle.dump(self.mouse_check, file)
+            
+            track_name='D='+str(self.inf['D'])+'_W='+str(self.inf['W'])+"_track.bin"
+            track_path=sys.path[0]+'\data\\'+track_name
+            
+            divide_index=[]#记录鼠标移动和鼠标点击的分界点
+            print('点击点的个数为：',len(self.mouse_check))
+            for i in range(len(self.mouse_check)):
+                for j in range(len(self.mouse_move)):
+                    if self.mouse_check[i][0]<self.mouse_move[j][0]:
+                        divide_index.append(j)
+                        break
+            #得到切片的索引
+            self.track=[]
+            for i in range(0,len(divide_index)-1,2):
+                self.track.append(self.mouse_move[divide_index[i]:divide_index[i+1]])
+            
+            print('总的轨迹长度为：',len(self.mouse_move))
+            
+            for i in range(len(self.track)):
+                print(len(self.track[i]))
+
+            with open(track_path, "wb") as file:
+                pickle.dump(self.track, file)
+            
+            messagebox.showinfo("Save File", "数据保存成功")
+              
         self.geme_window = Toplevel(self.root)
         self.geme_window.focus_set()
         # 获取屏幕尺寸
@@ -197,50 +252,6 @@ class App:
             self.game_canvas.delete(item_id)  # 删除该项
             self.mouse_check.append((time.time(),(event.x,event.y)))#只记录点击到的点的位置和时间
         
-        # 保存数据并退出
-        def save_esc(event):
-            #保存实验的关键信息
-            information_name = 'D='+str(D)+'_W='+str(W)+"_information.bin"  # 文件路径和名称
-            information_path=sys.path[0]+'\data\\'+information_name
-            # 打开文件，以写入模式
-            with open(information_path, "wb") as file:
-                pickle.dump(self.inf, file)
-            
-            mouse_move_name = 'D='+str(D)+'_W='+str(W)+"_mouse_move.bin"  # 文件路径和名称
-            mouse_move_path=sys.path[0]+'\data\\'+mouse_move_name
-            # 打开文件，以写入模式
-            with open(mouse_move_path, "wb") as file:
-                pickle.dump(self.mouse_move, file)
-            
-            mouse_check_name='D='+str(D)+'_W='+str(W)+"_mouse_check.bin"
-            mouse_check_path=sys.path[0]+'\data\\'+mouse_check_name
-            with open(mouse_check_path, "wb") as file:
-                pickle.dump(self.mouse_check, file)
-            
-            track_name='D='+str(D)+'_W='+str(W)+"_track.bin"
-            track_path=sys.path[0]+'\data\\'+track_name
-            
-            divide_index=[]#记录鼠标移动和鼠标点击的分界点
-            print('点击点的个数为：',len(self.mouse_check))
-            for i in range(len(self.mouse_check)):
-                for j in range(len(self.mouse_move)):
-                    if self.mouse_check[i][0]<self.mouse_move[j][0]:
-                        divide_index.append(j)
-                        break
-            #得到切片的索引
-            track=[]
-            for i in range(0,len(divide_index)-1,2):
-                track.append(self.mouse_move[divide_index[i]:divide_index[i+1]])
-            
-            print('总的轨迹长度为：',len(self.mouse_move))
-            
-            for i in range(len(track)):
-                print(len(track[i]))
-   
-            with open(track_path, "wb") as file:
-                pickle.dump(track, file)
-            
-            self.geme_window.destroy()
         
         def create_cirs():
             if self.cir==0:
@@ -257,14 +268,20 @@ class App:
                 pass
             #是否结束测试
             if self.show_times==2:
-                    save_esc(1)
+                    save_esc()
+                    self.geme_window.destroy()
+                    return
             self.geme_window.after(1000, create_cirs)
             
         create_cirs()
-        # 隐藏窗口边框
+       
+        def exit_game(event):
+            save_esc()
+            self.geme_window.destroy()
+            return
         
         #按1退出
-        self.geme_window.bind('1', save_esc)
+        self.geme_window.bind('1', exit_game)
 
         # 绑定鼠标事件
         self.mouse_move=[]
@@ -280,27 +297,22 @@ class App:
 
         record_mouse_position()
 
-        
-
-
     def show_trace(self):
         self.canvas.delete('all')
+        try:
+            self.mouse_move,self.mouse_check,self.track,self.inf=self.read_data(self.D.get(),self.W.get())
+        except:
+            messagebox.showerror(title='错误',message='没有找到数据')
+            return None
         
-        mouse_check_path=sys.path[0]+'\data\D=2000_W=100_mouse_check.bin'
-        mouse_move_path=sys.path[0]+'\data\D=2000_W=100_mouse_move.bin'
-        information_path=sys.path[0]+'\data\D=2000_W=100_information.bin'
-        track_path=sys.path[0]+'\data\D=2000_W=100_track.bin'
+        inf=self.inf
 
-        with open(information_path,'rb') as file:
-            inf=pickle.load(file)
-        with open(track_path,'rb') as file:
-            track=pickle.load(file)
-        with open(mouse_check_path,'rb') as file:
-            mouse_check=pickle.load(file)
+        track=self.track
+
+        mouse_check=self.mouse_check
     
-        
-        D=int(self.D.get())
-        W=int(self.W.get())
+        D=inf['D']
+        W=inf['W']
         screen_width=inf['screen_width']
         screen_height=inf['screen_height']
         
@@ -322,7 +334,7 @@ class App:
                 self.line=0
                 return 
             point=track[self.line][self.times][1]
-            self.canvas.create_oval(point[0]*k_x,point[1]*k_y,(point[0]+10)*k_x,(point[1]+10)*k_y, fill=colors[self.line],tags='point')
+            self.canvas.create_oval(point[0]*k_x,point[1]*k_y,(point[0]+5)*k_x,(point[1]+5)*k_y, fill=colors[self.line],tags='point',)
             self.root.after(1,after_1s)
             self.times+=1
             #print(times,len(track[line]))
